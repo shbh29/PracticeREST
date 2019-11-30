a86c767148c36cb9bc7e0b5b2507356cf5b878e5
Shubham <shbh29@gmail.com>
2019-11-30 21:27:46 +0530

    First Working REST API.

---
+++ src/main/java/com/shbh/practice/messenger/service/PersonService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shbh.practice.messenger.service;
+
+import com.shbh.practice.messenger.model.Person;
+import com.shbh.practice.messenger.model.Response;
+
+
+public interface PersonService {
+
+	public Response addPerson(Person p);
+	
+	public Response deletePerson(int id);
+	
+	public Person getPerson(int id);
+	
+	public Person[] getAllPersons();
+
+}
